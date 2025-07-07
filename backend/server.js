40d4618bd3232253c3e94b4130d74d68e13a12d7
zehra.yatki <zehra.yatki@gmail.com>
2025-07-07 14:09:39 +0300

    bug fix front and backend entegration

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:4173",
       "http://localhost:3000",
+      "https://reservee-ke9r.onrender.com",
       "https://your-frontend-domain.com",
     ],
     credentials: true,
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/reservee/",
+  base: "./",
   build: {
     outDir: "dist",
     assetsDir: "assets",
